71913b1ff1070367305b4ad6703ed9889c6eb176
talhaustundag <talhaustundag51@gmail.com>
2025-11-16 00:36:06 +0300

    v8
    Product controlleri yazıldı
    Listeleme, ekleme, düzenleme ve silme gibi fonksiyonlar aktif
    Route yolları düzenlendi

---
+++ database/migrations/2025_11_15_210723_update_products_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->decimal('price', 10, 2)->change();
+            $table->renameColumn('stock_quantity', 'stock');
+            $table->unsignedBigInteger('category_id')->nullable(false)->change();
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->string('brand')->nullable();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,13 +21,21 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('login',[LoginController::class,'login']);
+Route::post('register',[LoginController::class,'register']);
+
 Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/products', [ProductController::class, 'index']);
+
 // Admin routes
 Route::middleware(['auth:sanctum', 'is_admin'])->group(function () {
+    //Kategori
     Route::post('/categories', [CategoryController::class, 'store']);
     Route::put('/categories/{category}', [CategoryController::class, 'update']);
     Route::delete('/categories/{category}', [CategoryController::class, 'destroy']);
+    //ÜRÜNLER
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{product}', [ProductController::class, 'update']);
+    Route::delete('/products/{product}', [ProductController::class, 'destroy']);
 });
 
-Route::post('login',[LoginController::class,'login']);
-Route::post('register',[LoginController::class,'register']);

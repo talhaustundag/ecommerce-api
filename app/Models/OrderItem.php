5827b3a6abec67746f74dfa9d895700db0f523bb
talhaustundag <talhaustundag51@gmail.com>
2025-11-16 13:29:12 +0300

    Order Kontrolleri
    Order ve OrderItem tabloları düzenlendi
    OrderController oluşturuldu
    Route yolları girildi ve kontrolleri yazıldı

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class OrderItem extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'order_id',
+        'product_id',
+        'quantity',
+        'price'
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
